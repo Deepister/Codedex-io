e4bfc8b847dc53ce5cc3d531539b1a99e9b63061
deepi <deepister.mgmt@gmail.com>
2024-05-06 12:31:44 -0700

    addded a python folder and a test project

---
+++ Python/initials.py
@@ -0,0 +1,9 @@
+# Create an initials.py program that displays your initials in block letters.
+
+print("DDDD  PPPP")
+print("D   D P  P")
+print("D   D P  P")
+print("D   D PPPP")
+print("D   D P   ")
+print("D   D P   ")
+print("DDDD  P   ")
