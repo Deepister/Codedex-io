2dbfe355e3db5ea979b52c9fda46eab44e07b075
deepi <deepister.mgmt@gmail.com>
2024-05-06 20:03:21 -0700

    added gradingcurve project

---
+++ Python/gradeingCurve.py
@@ -0,0 +1,12 @@
+# Create a grades.py program that checks whether a grade is above or below 55.
+
+import random
+num = random.randint(0,100)
+
+grade = num
+
+if grade >= 55:
+  print("You Passed!")
+else:
+  print("You Failed")
+
